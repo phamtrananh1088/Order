bee71320e8c254fa46c6b82c7ea42099f60b9a1d
phamtrananh1088 <phamtrananh1088@gmail.com>
2024-11-05 18:16:01 +0700

    Initial Commit

---
+++ Order/OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderApp.swift
+//  Order
+//
+//  Created by anh on 2024/11/05.
+//
+
+import SwiftUI
+
+@main
+struct OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
